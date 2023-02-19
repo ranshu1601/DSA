032b0e514494713f568a9a385774b7cdf6262288
Anshu Ranjan <53359975+ranshu1601@users.noreply.github.com>
2023-02-19 10:01:41 +0530

    Add files via upload

---
+++ DSA/Recursion/myFirst.cpp
@@ -0,0 +1,22 @@
+// myFirst.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+
+#include <iostream>
+using namespace std;
+
+int fun(int n){
+	static int x = 0 ;
+	if(n>0){
+		x++;
+		return fun(n-1)+x;
+		
+	}
+	return 0 ;
+}
+
+int main(){
+	int r;
+	r=fun(5);
+	cout<<r;
+	return 0 ; 
+}
