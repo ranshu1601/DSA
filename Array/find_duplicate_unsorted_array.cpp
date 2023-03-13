7b22d72a498f1ce885f61d99c076842ea9490453
Anshu Ranjan <lowkeyreal247@gmail.com>
2023-03-13 22:07:26 +0530

    13

--- Array/find_duplicate_unsorted_array.cpp
+++ Array/find_duplicate_unsorted_array.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main(){
-    int n;
-    cout<<"enter the no. of elements in an array(n): ";
-    cin>>n;
-    int A[n];
-    cout<<" Array emelemts -> "<<endl;
-    for(int i=0;i<n;i++){
-        cout<<"enter "<<i<<" element: ";
-        cin>>A[i];
-    }
+// int main(){
+//     int n;
+//     cout<<"enter the no. of elements in an array(n): ";
+//     cin>>n;
+//     int A[n];
+//     cout<<" Array emelemts -> "<<endl;
+//     for(int i=0;i<n;i++){
+//         cout<<"enter "<<i<<" element: ";
+//         cin>>A[i];
+//     }
 
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if (A[i]==A[j]){
-                cout<<A[i]<<endl ;
-            }
-        }
-    }
+//     for(int i=0;i<n;i++){
+//         for(int j=i+1;j<n;j++){
+//             if (A[i]==A[j]){
+//                 cout<<A[i]<<endl ;
+//             }
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 
